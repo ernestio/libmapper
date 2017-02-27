331204cdccd6a342f2ac4b2b6f5a341036ff9eb7
Adrià Cidre <adria.cidre@gmail.com>
2017-02-27 10:27:06 +0100

    Some documentation on mapper interface

--- mapper.go
+++ mapper.go
@@ -2,11 +2,26 @@ package libmapper
 
 import "github.com/r3labs/graph"
 
-// Mapper : interface for each provider mapper to satisfy
+// Mapper : interface for each provider mapper to satisfy. A mapper
+// is basically managing all translations between a Definition (the
+// user interface of an ernest service) and the Graph (the internal
+// representation of this service).
+// Additionally Graph object includes the necessary steps to build a
+// specific service
 type Mapper interface {
+
+	// ConvertDefinition : Given the input Definition it returns a valid Graph ("service") object
 	ConvertDefinition(Definition) (*graph.Graph, error)
+
+	// ConvertGraph : Given a valid Graph("service") object it will build a valid Definition
 	ConvertGraph(*graph.Graph) (Definition, error)
+
+	// LoadDefinition : ...
 	LoadDefinition(map[string]interface{}) (Definition, error)
+
+	// LoadGraph : ...
 	LoadGraph(map[string]interface{}) (*graph.Graph, error)
+
+	// CreateImportGraph : Creates an import graph based on the specified service_id
 	CreateImportGraph(string) *graph.Graph
 }
